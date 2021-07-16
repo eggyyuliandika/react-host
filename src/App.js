9778f6aafefb2ab4712ad90756dbb9b76f3cd6f2
eggyyuliandika <eggyyuliandika@gmail.com>
2021-07-16 19:47:55 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import List from "./List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItem: "",
+      items: [],
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.todoItem],
+      todoItem: "",
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value,
+    });
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({
+      items: [""],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>To-Do List</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="input"
+            value={this.state.todoItem}
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            placeholder="Ketikkan sesuatu..."
+          />
+          <button>Add</button>
+        </form>
+
+        <form onClick={this.handleClick}>
+          <button>Clear</button>
+        </form>
+
+        <List items={this.state.items} />
+      </div>
+    );
+  }
 }
 
 export default App;
